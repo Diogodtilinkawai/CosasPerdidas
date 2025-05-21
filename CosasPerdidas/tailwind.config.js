cbe50bcdfbc190f0b50876491be2c3d2d6a9941c
paolalisssv <157732009+paolalisssv@users.noreply.github.com>
2025-05-21 14:13:30 -0600

    fix tailwind

---
+++ CosasPerdidas/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // Asegúrate de que esto incluya todos los archivos relevantes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
